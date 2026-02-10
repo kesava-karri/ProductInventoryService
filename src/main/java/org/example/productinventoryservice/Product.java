090f5917b3d98eb43d329c75597dc718ab5014ba
Keshav K <kkeshav1998@gmail.com>
2026-02-11 00:50:51 +0530

    ProductInventoryService: Create basic Entity

---
+++ src/main/java/org/example/productinventoryservice/Product.java
@@ -0,0 +1,27 @@
+package org.example.productinventoryservice;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="PRODUCT_NAME", length=50, nullable=false, unique=false)
+    private String productName;
+
+    private Double price;
+
+    private Integer stockQuantity;
+
+    @Version
+    private Long version;
+}
---
+++ src/test/java/org/example/productinventoryservice/ProductInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
